72dbe50484b7b62fd4d9ed885d946261d7b31d1b
Theelx <43764914+Theelx@users.noreply.github.com>
2022-12-04 18:50:31 -0500

    testing

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ requirements = [
         'packaging',    # For parsing versions
         'gitpython',    # Git version information
         'jellyfish',    # For fuzzy string matching
-        'line_profiler ;   platform_system == "Linux"',   # For the line profiler -- only install on Linux
+        'git+https://github.com/Theelx/line_profiler.git ;   platform_system == "Linux"',   # For the line profiler -- only install on Linux
         'memory_profiler ; platform_system == "Linux"',   # For the memory profiler -- only install on Linux
         'colorama ;        platform_system == "Windows"', # For colored text output -- only install on Windows
         ]
